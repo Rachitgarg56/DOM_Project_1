9edca57c303222799da581832b5084ac6f6694fa
Rachit Garg <rachitgarg56@gmail.com>
2024-01-11 16:03:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const div = document.getElementById('div');
+
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
+
+div.style.color = "blue";
+div.classList.add('container');
+
+console.log(div);
